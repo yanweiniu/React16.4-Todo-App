6be2b520f75b0ecdf0160a5787dc11326aceb7bb
yanwei <2804695074@qq.com>
2022-06-10 11:00:22 +0800

    Initial commit

---
+++ src/Components/TodoList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import './component.css';
+
+class TodoList extends React.Component {
+
+    render() {
+    return (  
+        <div>
+        {this.props.items.map(item => (
+        <TodoItem id={item.id} status={item.status} task={item.task} deleteItem={this.props.deleteItem} markItemComplete={this.props.markItemComplete} />
+        ))} 
+    </div>
+        
+    );
+  } 
+}
+
+export default TodoList;
+
